73d6ab616de5b19c7e9f454b3a68cc4d7f07599d
rakeshakrs <rakeshakrs@gmail.com>
2024-10-30 19:20:57 +0530

    Example 8 - wiring beans using method call

---
+++ example8/src/main/java/com/example/config/ProductConfig.java
@@ -0,0 +1,29 @@
+package com.example.config;
+
+import com.example.beans.Person;
+import com.example.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@ComponentScan(basePackages = "com.example.beans")
+public class ProductConfig {
+
+    @Bean
+    public Vehicle vehicle(){
+        Vehicle vehicle=new Vehicle();
+        vehicle.setName("Vehicle1");
+        return vehicle;
+    }
+
+    @Bean
+    public Person person(){
+        Person person=new Person();
+        person.setName("Person1");
+        person.setVehicle(vehicle());
+        return person;
+    }
+
+}
