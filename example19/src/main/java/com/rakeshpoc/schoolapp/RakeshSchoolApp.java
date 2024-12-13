b58e850722a2607546056a68b238499ae2ea8e47
rakeshakrs <rakeshakrs@gmail.com>
2024-12-13 09:43:38 +0530

    example 19 - Hello World Web App using SpringBoot & Spring MVC

---
+++ example19/src/main/java/com/rakeshpoc/schoolapp/RakeshSchoolApp.java
@@ -0,0 +1,13 @@
+package com.rakeshpoc.schoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RakeshSchoolApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RakeshSchoolApp.class, args);
+	}
+
+}
